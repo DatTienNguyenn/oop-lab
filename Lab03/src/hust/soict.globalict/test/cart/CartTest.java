dae0a75e4e41e286e73f9a11b383a79953e1c28b
Ngtiendat <dn841746@gmail.com>
2024-04-26 11:54:40 +0700

    Complete Assignment

---
+++ Lab03/src/hust/soict.globalict/test/cart/CartTest.java
@@ -0,0 +1,37 @@
+
+package hust.soict.globalict.test.cart;
+
+import hust.soict.globalict.aims.cart.Cart;
+import hust.soict.globalict.aims.disc.DigitalVideoDisc;
+import hust.soict.globalict.aims.media.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartTest {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", 
+            "Animation", "Roger Allers", 87, 19.95f);
+        cart.addMedia(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", 
+            "Science Fiction", "George Lucas", 87, 24.95f);
+        cart.addMedia(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
+            "Animation", 18.99f);
+        cart.addMedia(dvd3);
+
+        List<String> authors = new ArrayList<String>();
+        authors.add("LTK");
+        Book book1 = new Book(1, "Sherlock Holmes", "Thriller", 20.99f, authors);
+        cart.addMedia(book1);
+
+        cart.toString();
+        cart.printList();
+    }
+    
+}
+
